2d89c1ba0263056b7cbe5f9e15371d7af0104513
Federico Airoldi <airoldibusiness@gmail.com>
2024-01-30 01:12:53 +0100

    handle Set Name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,29 +1,48 @@
+import { useState } from "react";
+
 export default function Player() {
+  let buttonClass =
+    "py-1 px-2 whitespace-nowrap teinline-flex justify-center items-center  text-sm font-semibold rounded-e-md bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600";
+  let InputClass =
+    "py-1 ps-2 block w-full  shadow-sm rounded-s-lg text-sm focus:outline-none disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600";
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(e) {
+    setSubmitted(false);
+    setEnteredPlayerName(e.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section className="w-[48rem] h-auto bg-gray-950 rounded-lg mt-20 p-6 flex justify-top flex-col items-center shadow-xl">
       <h1 className="font-black text-3xl">
-        THE <span className="text-blue-400">ALMOST</span> FINAL COUNT DOWN
+        THE <span className="text-blue-500">ALMOST</span> FINAL COUNT DOWN
       </h1>
       <span className="text-gray-500">
         Stop the timer once you estimate that times is (almost) up
       </span>
 
       <div className="mt-4 flex flex-col gap-4 text-xl text-emerald-600">
-        <label className="font-bold">Welcome unknown entity</label>
-        <div className="relative">
-          <input
-            type="search"
-            id="search"
-            className=" w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder=""
-            required
-          />
-          <button
-            type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Set Name
-          </button>
+        <label className="font-bold">
+          Welcome {submitted ? enteredPlayerName : "unknown entity"}
+        </label>
+        <div>
+          <div className="flex rounded-lg shadow-sm">
+            <input
+              type="text"
+              className={InputClass}
+              onChange={handleChange}
+              value={enteredPlayerName}
+            />
+            <button className={buttonClass} onClick={handleClick}>
+              Set Name
+            </button>
+          </div>
         </div>
       </div>
     </section>
