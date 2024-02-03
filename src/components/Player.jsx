1c18968c84a38f2e1bbfde07b0aefe20cf189934
Federico Airoldi <airoldibusiness@gmail.com>
2024-02-03 16:16:03 +0100

    minor changes

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -16,7 +16,7 @@ export default function Player() {
   }
 
   return (
-    <section className="w-[48rem] h-auto bg-gray-950 rounded-lg mt-20 p-6 flex justify-top flex-col items-center shadow-xl">
+    <section className="w-[48rem] h-auto bg-gray-950 rounded-lg mt-16 p-6 flex justify-top flex-col items-center shadow-xl">
       <h1 className="font-black text-3xl ">
         THE <span className="text-blue-500">ALMOST</span> FINAL COUNT DOWN
       </h1>
